9e6c934643ad9e44c0417ce3c3831369bc8e3b1a
hamuyuuki <13702378+hamuyuuki@users.noreply.github.com>
2020-05-07 17:02:23 +0900

    Apliy migration versioning to fixtures ref: https://github.com/rails/rails/pull/21538

--- spec/fixtures/db/migrate/20140108194650_create_test_rake.rb
+++ spec/fixtures/db/migrate/20140108194650_create_test_rake.rb
@@ -1,4 +1,4 @@
-class CreateTestRake < ActiveRecord::Migration
+class CreateTestRake < ActiveRecord::VERSION::MAJOR >= 5 ? ActiveRecord::Migration::Current : ActiveRecord::Migration
   def change
     create_table :test_rake
   end
